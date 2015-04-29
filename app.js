0d9eb0bf9c411bef88bfe27a6f39ca7aa5e3051c
Yüce Çelikel <yucecelikel@Yuces-MacBook-Pro.local>
2015-04-29 15:23:06 +0300

    remove hostname from listen

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var config = require('./config/config'),
-  glob = require('glob')
   http = require('http');
   folder = require('./folder');
 
 http.createServer(function(request, response) {
   folder(request, response);
-}).listen(config.port, "127.0.0.1");
+}).listen(config.port);
 console.log("server started at http://127.0.0.1:" + config.port + "/");
