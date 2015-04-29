a2798b2f5b62e7369675e82eab6c8c144237183f
Yüce Çelikel <yucecelikel@Yuces-MacBook-Pro.local>
2015-04-29 09:04:12 +0300

    writing to project folder

--- folder.js
+++ folder.js
@@ -1,17 +1,16 @@
 var config = require('./config/config'),
   fs = require('fs');
+  url = require('url');
 
 var saveFile = function(request, response) {
   var writer = fs.createWriteStream("file.jpg");
-  request.pipe(writer, { end: false });
-  request.on('end', function() {
-    writer.end();
+  request.pipe(writer);
 
+  writer.on('finish', function() {
     response.writeHead(201, { "Content-Type": "text/plain" });
     response.end("created");
   });
 };
-
 
 module.exports = function(request, response) {
   if(request.method == "POST") {
